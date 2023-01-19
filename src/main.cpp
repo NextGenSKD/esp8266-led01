80facb2a682928a990edb54684c64a1b25d2da06
NextGenSKD <it@ngskd.com>
2023-01-19 16:49:18 +0300

    Running stream 14comb+stars  ver 0.1.6

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 CRGB leds[LED_NUM];
 
-byte counter = 0;
+int counter = 0;
 int col_b;
 int col_r;
 int col_g;
@@ -15,6 +15,19 @@ byte combi2 = 0;
 int uskor =1;
 bool c_flag = true;
 bool combi_flag = true;
+byte bright = 64;
+bool test = false;
+bool stars = false;
+byte counterbyte;
+struct test_st
+{
+  byte b1;
+  byte b2;
+  byte b3;
+};
+test_st t;
+
+
 
 void select_anim (byte combix)
 {
@@ -33,15 +46,9 @@ void select_anim (byte combix)
           if (combix == 13) {col_b=128; col_g=128; col_r=128;};
 }
 
-void setup() {
-  Serial.begin(921600);
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
-  FastLED.setBrightness(50);
-  Serial.println("SETUP! Success!");
-}
 
-void loop() {
-  // Debug info
+void debug()   {
+// Debug info
   Serial.print("\r Counter:");
   Serial.print(counter);
   Serial.print(" c_flag:");
@@ -56,8 +63,28 @@ void loop() {
   Serial.print(combi_flag);
   Serial.print(" uskor:");
   Serial.print(uskor);
+  Serial.print(" t.b1:");
+  Serial.print(t.b1);
+  Serial.print(" t.b2:");
+  Serial.print(t.b2);
+  Serial.print(" t.b3:");
+  Serial.print(t.b3);
   Serial.print("   ");
-  // End Debug  
+
+
+  // End Debug
+  }  
+
+void setup() {
+  Serial.begin(921600);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, LED_NUM);
+  FastLED.setBrightness(20);
+  Serial.println("SETUP! Success!");
+ //Serial.print("[31;40m");
+}
+
+void loop() {
+
  uskor = abs (sin(counter)*5);
  if (counter>= LED_NUM) {c_flag=false; combi=combi2;  combi2=random(13);}
  if (counter<= 0) {c_flag=true; combi1=combi2; combi=combi; combi2=random(13);}
@@ -66,11 +93,11 @@ void loop() {
  //if (combi== 0) combi_flag=true;
    //  Serial.print("  col_b: ");
   //  Serial.print(col_b);
-    for (int i=0;i<=counter;i++) 
+   for (int i=0;i<=counter;i++) 
         {
-          col_b = 64+sin(i/10)*64;
-          col_r = 64+sin(i+counter/10)*64;
-          col_g = 64+cos(i+counter/10)*64;
+          col_b = bright+sin(i/10)*bright;
+          col_r = bright+sin(i+counter/10)*bright;
+          col_g = bright+cos(i+counter/10)*bright;
           //if (c_flag) col_g=col_r; else col_g=col_b;
           select_anim (combi1);
 
@@ -81,26 +108,37 @@ void loop() {
     
     for (int i=counter;i<=LED_NUM;i++) 
         {
-          col_b = 64+sin(i/10-counter/10)*64;
-          col_r = 64+cos(i/10+counter/10)*64;
-          col_g = 64+cos(i/10)*100;
+          col_b = bright+sin(i/10-counter/10)*bright;
+          col_r = bright+cos(i/10+counter/10)*bright;
+          col_g = bright+cos(i/10)*bright;
           //if (c_flag) col_g=col_b; else col_g=col_r;
           select_anim (combi);
  
           leds[i] = col_b+(col_r<<8)+(col_g<<16);
           //Serial.print(" ");
           //Serial.print(col_b);
-          }
-       for (int i=0;i<=10;i++)   {
-      leds[random(LED_NUM)] = 128+(128<<8)+(128<<16);
-       }
-        /*
+          } 
+    // Stars
+    if (stars) {       
+      for (int i=0;i<=10;i++)   {
+          leds[random(LED_NUM)] = 128+(128<<8)+(128<<16);
+            }
+        }   //end start  
+      
         //For test brightness     
-        for (int i=0;i<=LED_NUM;i++) 
-          { int col = 120+sin(i)*64;
-            leds[i] = 0+(col<<8)+(0<<16);} 
-        // */
+      if (test) {
+         for (int i=0;i<=LED_NUM;i++) 
+          { //if (counterbyte<=)
+            
+            //leds[i] = col_b+(col_r<<8)+(col_g<<16);} 
+            leds[i] = i+(i<<8)+(i<<16);} 
+                }
+        // end test brightness
   FastLED.show();
-  delay(100);
+   //delay(100);
+  debug();
   FastLED.clear();
+  counterbyte++;
+  t.b2++;
+
 }
